2de75b037286c3a0e76ed25f46ac17ad76556e3b
CLOUDFRAMEWORK <devteam@cloudframework.io>
2020-03-11 06:24:27 +0100

    Adding Twig support for version v73.03111

--- src/Core7.php
+++ src/Core7.php
@@ -98,7 +98,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
 
-        var $_version = 'v73.03101';
+        var $_version = 'v73.03111';
 
         /**
          * @var array $loadedClasses control the classes loaded
