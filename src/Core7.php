c1b3621696d2505caf494d333a0601fad42a2e68
CLOUDFRAMEWORK <devteam@cloudframework.io>
2020-03-29 09:20:59 +0200

    Fixing update/insert data with % character and upgrading to v73.03291

--- src/Core7.php
+++ src/Core7.php
@@ -98,7 +98,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
 
-        var $_version = 'v73.03272';
+        var $_version = 'v73.03291';
 
         /**
          * @var array $loadedClasses control the classes loaded
