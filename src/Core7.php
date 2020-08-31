db7402a0b44adb04214bf10e6f70e82b0af0e4bc
CLOUDFRAMEWORK <devteam@cloudframework.io>
2020-08-31 12:15:40 +0200

    Addring readModelsFromCloudFramework v73.08311

--- src/Core7.php
+++ src/Core7.php
@@ -100,7 +100,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
 
-        var $_version = 'v73.08301';
+        var $_version = 'v73.08311';
 
         /**
          * @var array $loadedClasses control the classes loaded
@@ -4125,6 +4125,26 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
         }
 
         /**
+         * Read models from CloudFramework
+         * @param $models string Models separated by ,
+         * @param $api_key string API Key of the licence
+         * @param int $cache integer If you want to use cache thet it hast to be > 0
+         * @return boolean|void
+         */
+        function readModelsFromCloudFramework($models,$api_key,$cache_time=0) {
+            $ret_models = [];
+            if($cache_time) $ret_models = $this->core->cache->get('readModelsFromCloudFramework_'.$models,$cache_time);
+            if(!$ret_models) {
+                $ret_models =  $this->core->request->get_json_decode('https://api7.cloudframework.io/core/models/export',['models'=>$models],['X-WEB-KEY'=>$api_key]);
+                if($this->core->request->error)  return($this->addError($this->core->request->errorMsg));
+                $ret_models = $ret_models['data'];
+                $this->core->cache->set('readModelsFromCloudFramework_'.$models,$ret_models);
+            }
+            if(!$ret_models || !$this->processModels($ret_models)) return;
+            return true;
+        }
+
+        /**
          * Process the model received in models
          * @param $models array
          */
@@ -4144,6 +4164,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
                 foreach ($models['JSONTables'] as $model=>$dsEntity) {
                     $this->models['json:'.$model] = ['type'=>'json','data'=>$dsEntity];
                 }
+            return true;
         }
 
         /**
