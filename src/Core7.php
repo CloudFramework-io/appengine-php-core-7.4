c27c61593b93670522c9db69c194cb4377f79ffc
CLOUDFRAMEWORK <devteam@cloudframework.io>
2020-09-17 06:01:48 +0200

    Evolving email processing to mailparse_xbateson zbateson/mail-mime-parser adding library in composer.json v73.09171

--- src/Core7.php
+++ src/Core7.php
@@ -100,7 +100,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
 
-        var $_version = 'v73.09061';
+        var $_version = 'v73.09171';
 
         /**
          * @var array $loadedClasses control the classes loaded
