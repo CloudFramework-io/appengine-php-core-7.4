e4510e8270125c74e17a0e3b65b389ab294596d2
CLOUDFRAMEWORK <devteam@cloudframework.io>
2020-10-09 09:11:40 +0200

    Evolving CloudFrameworkTests

--- src/Core7.php
+++ src/Core7.php
@@ -100,7 +100,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
 
-        var $_version = 'v73.10081';
+        var $_version = 'v73.10091';
 
         /**
          * @var array $loadedClasses control the classes loaded
