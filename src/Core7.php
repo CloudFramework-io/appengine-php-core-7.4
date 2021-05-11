5000c50b77da641a2a765508ffb98fc8d92df0d7
Adrian at BNEXT <am@bnext.es>
2021-05-11 08:30:28 +0200

    Upgrade to: v73.17111

--- src/Core7.php
+++ src/Core7.php
@@ -97,7 +97,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
 
-        var $_version = 'v73.17071';
+        var $_version = 'v73.17111';
 
         /**
          * @var array $loadedClasses control the classes loaded
