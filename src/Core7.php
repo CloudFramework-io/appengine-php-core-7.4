b402cbb057a70428be775b3e763a8358f552d455
CLOUDFRAMEWORK <bnext@cloudframework.io>
2021-10-06 09:02:44 +0200

    Updating to: v73.22061

--- src/Core7.php
+++ src/Core7.php
@@ -97,7 +97,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
 
-        var $_version = 'v73.21211';
+        var $_version = 'v73.22061';
 
         /**
          * @var array $loadedClasses control the classes loaded
