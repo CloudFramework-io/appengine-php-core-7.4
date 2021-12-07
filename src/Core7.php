57fac17df695e4dd0ddfc39fccab8bf70022ba3a
CLOUDFRAMEWORK <bnext@cloudframework.io>
2021-12-07 06:49:50 +0100

    Upgrading v73.00071 Migrating to php74

--- src/Core7.php
+++ src/Core7.php
@@ -106,7 +106,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = 'v73.24062';
+        var $_version = 'v73.00071';
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
