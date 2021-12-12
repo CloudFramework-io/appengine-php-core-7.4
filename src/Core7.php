e9dee5c58ea8158e739c3a389b4df658ee6601ca
CLOUDFRAMEWORK <bnext@cloudframework.io>
2021-12-12 11:15:17 +0100

    Improving first steps installation Upgrade to v74.00124

--- src/Core7.php
+++ src/Core7.php
@@ -111,7 +111,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = 'v74.00123';
+        var $_version = 'v74.00124';
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
