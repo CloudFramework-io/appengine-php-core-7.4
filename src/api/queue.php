8c83ef5a8ce943588dea688edae7ffa5d334b5c6
CLOUDFRAMEWORK <devteam@cloudframework.io>
2023-05-15 10:59:59 +0200

    Adding security of the url when is call /queue

--- src/api/queue.php
+++ src/api/queue.php
@@ -36,15 +36,19 @@ class API extends RESTful
         $this->sendCorsHeaders();
         if(isset($this->formParams['_raw_input_'])) unset($this->formParams['_raw_input_']);
 
+        // In interactive we use CloudService Class to send and receive data with http...
+        //$_url = str_replace('/queue/', '/', urldecode($this->core->system->url['host_url_uri']));
+        if(!strpos($this->core->system->url['host_url_uri'],'/queue/'))
+            return $this->setErrorFromCodelib('params-error','call does not include /queue/. pattern');
+        if(strpos($this->core->system->url['host_url_uri'],'/queue/queue'))
+            return $this->setErrorFromCodelib('params-error','call does not allow /queue/queue. pattern');
+        $_url = str_replace('/queue/', '/', ($this->core->system->url['host_url_uri']));
+
         // CALL URL and wait until the response is received
         if (isset($this->formParams['_interactive'])) {
 
             //Delete variable
             unset($this->formParams['interactive']);
-
-            // In interactive we use CloudService Class to send and receive data with http...
-            //$_url = str_replace('/queue/', '/', urldecode($this->core->system->url['host_url_uri']));
-            $_url = str_replace('/queue/', '/', ($this->core->system->url['host_url_uri']));
 
             // Requires to create a complete URL
             $value['url_queued'] = $_url;
@@ -99,10 +103,7 @@ class API extends RESTful
             $this->formParams['cloudframework_queued_fingerprint'] = json_encode($this->core->system->getRequestFingerPrint(), JSON_PRETTY_PRINT);
             //endregion
 
-            //region SET '$_url'
-            //$_url = str_replace('/queue/', '/', urldecode($this->core->system->url['url_uri']));
-            $_url = str_replace('/queue/', '/', ($this->core->system->url['url_uri']));
-            //endregion
+
 
             //region CREATE $client, $queueName and $task to be queued
             $client = new CloudTasksClient();
