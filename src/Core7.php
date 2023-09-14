e9a1bfd5873deaeadee416f0e9cc914e956576e0
Héctor López CF <hlopez@cloudframework.io>
2023-09-14 07:17:24 +0200

    Improving GoogleDocuments class Adding CoreUser.addUserData with caching properties Upgrading to v74.21142

--- src/Core7.php
+++ src/Core7.php
@@ -156,7 +156,7 @@ if (!defined("_CLOUDFRAMEWORK_CORE_CLASSES_")) {
     final class Core7
     {
         // Version of the Core7 CloudFrameWork
-        var $_version = 'v74.21141';
+        var $_version = 'v74.21142';
         /** @var CorePerformance $__p */
         var  $__p;
         /** @var CoreIs $is */
